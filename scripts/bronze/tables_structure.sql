efa1a1e056dfcf0a254e3fb7edd7d1448cc52e8e
pgalhassan <pgalhassan1811@gmail.com>
2026-08-13 18:18:07 +0000

    Update tables_structure.sql

--- scripts/bronze/tables_structure.sql
+++ scripts/bronze/tables_structure.sql
@@ -1,1 +1,65 @@
+/* 
+Methode de chargement des tables est le "truncate and insert" ce qui explique le test de verificaion de l'existence de la table avant sa création
+*/
 
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT,
+	cst_key NVARCHAR (100),
+	cst_firstname NVARCHAR (100),
+	cst_lastname NVARCHAR (100),
+	cst_marital_status NVARCHAR (100),
+	cst_gndr NVARCHAR (100),
+	cst_create_date DATE
+);
+
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key NVARCHAR (100),
+	prd_nm NVARCHAR (100),
+	prd_cost INT,
+	prd_line NVARCHAR (100),
+	prd_start_dt NVARCHAR (100),
+	prd_end_dt DATE
+);
+
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num NVARCHAR (100),
+	sls_prd_key NVARCHAR (100),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+	cid NVARCHAR (100),
+	bdate DATE,
+	gen NVARCHAR (100)
+);
+
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101(
+	cid NVARCHAR (100),
+	cntry NVARCHAR (100)
+);
+
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2(
+	id NVARCHAR (100),
+	cat NVARCHAR (100),
+	subcat NVARCHAR (100) ,
+	maintenance NVARCHAR (100)
+);
